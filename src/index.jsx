98222e5f38d949cd0cbbf9106f36c9f7e0b497fa
이태희 <leetaehee0205@gmail.com>
2021-09-24 10:29:02 +0900

    add : redux store & redux middleware (redux-thunk)

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,20 @@
+import { applyMiddleware, createStore } from "redux";
+
 import App from "./App";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
+import ReduxThunk from "redux-thunk";
+import rootReducers from "@modules";
+
+// Redux 스토어 생성
+const store = createStore(rootReducers, applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
