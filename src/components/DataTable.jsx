b292acd69ab3a4d3cf809b7cac4e10a2c54bd2d3
이태희 <leetaehee0205@gmail.com>
2021-09-24 12:35:20 +0900

    create : DataTable component

---
+++ src/components/DataTable.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+
+function DataTable({ categories, data, keyID }) {
+  return (
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>#</th>
+          {Object.keys(categories).map((key, idx) => (
+            <th key={idx}>{categories[key]}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((d, idx) => {
+          return (
+            <tr key={d[keyID]}>
+              <td>{idx + 1}</td>
+              {Object.keys(categories).map((key, idx) => (
+                <td key={idx}>{d[key]}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+      <tfoot></tfoot>
+    </Table>
+  );
+}
+
+export default DataTable;
