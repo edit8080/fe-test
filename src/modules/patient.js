fc6347dca9814dcb28bfb8edd99dff78be43755b
이태희 <leetaehee0205@gmail.com>
2021-09-24 12:35:46 +0900

    update : patient data formatting

--- src/modules/patient.js
+++ src/modules/patient.js
@@ -1,13 +1,26 @@
+import { dateFormat } from "@utils/format";
 import { getPatients } from "@apis/patient";
 
 const LOADING = "patient/PATIENT_LOADING";
 const PATIENT_FETCH = "patient/PATIENT_FETCH";
 const ERROR = "patient/PATIENT_ERROR";
 
+/**
+ * 환자 정보를 GET하여 상태 관리
+ *
+ * @param {Objects} filters
+ */
 export const fetchPatients = (filters) => async (dispatch) => {
   try {
     dispatch({ type: LOADING });
     const patientsData = await getPatients(filters);
+
+    // 환자 정보 포맷팅
+    patientsData.patient.list = patientsData.patient.list.map((item) => ({
+      ...item,
+      birthDatetime: dateFormat(item.birthDatetime),
+      isDeath: item.isDeath ? "T" : "F",
+    }));
 
     dispatch({ type: PATIENT_FETCH, ...patientsData.patient });
   } catch (e) {
