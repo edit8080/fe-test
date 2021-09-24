ca19db6f277ab3bbb36bb17b79fbaea568a6609d
이태희 <leetaehee0205@gmail.com>
2021-09-24 09:50:07 +0900

    create : get patient api data

---
+++ src/apis/patient.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+/**
+ * 쿼리에 기반하여 환자 정보 GET
+ *
+ * @param {Object} filters 환자 리스트 쿼리
+ * @returns {Object} list : 환자 정보 / page : 페이지 크기 / totalLength : 전체 데이터 길이
+ */
+export const getPatients = async (filters) => {
+  try {
+    let query = "";
+    Object.keys(filters).map((key) =>
+      key ? (query += `?${key}=${filters[key]}`) : ""
+    );
+
+    const patients = await axios.get(
+      `${process.env.API_SERVER}/api/patient/list${query}`
+    );
+
+    return patients.data;
+  } catch (e) {
+    throw new Error("환자 리스트 Fetch 실패");
+  }
+};
