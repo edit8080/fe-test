58f2cac963b2e553c0e775b874c7510059375951
이태희 <leetaehee0205@gmail.com>
2021-09-24 15:48:12 +0900

    create : PatientPageSelectBox component

--- src/components/PatientPageSelectBox.jsx
+++ src/components/PatientPageSelectBox.jsx
@@ -7,11 +7,17 @@ import { useDispatch } from "react-redux";
 /**
  * 선택 박스를 통해 환자 데이터 페이지 길이 설정
  *
- * @param {Object} props - page : 페이지 번호, selectItem : 선택 박스 항목
+ * @param {Object} props - page : 페이지 번호
  * @returns {HTMLElement} 환자 데이터 페이지 선택 박스
  */
-function PatientPageSelectBox({ page, selectItem }) {
+function PatientPageSelectBox({ page }) {
   const dispatch = useDispatch();
+
+  const selectItem = [
+    { val: 10, innerText: "10개씩 보이기" },
+    { val: 20, innerText: "20개씩 보이기" },
+    { val: 40, innerText: "40개씩 보이기" },
+  ];
 
   const pageChange = useCallback(
     (e) => {
