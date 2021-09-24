9ccc72da7e87c1181eee0cfd04ab393af46395bf
이태희 <leetaehee0205@gmail.com>
2021-09-24 23:18:49 +0900

    update : add patient pie charts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 
+import { Container } from "react-bootstrap";
+import PatientChartSet from "@components/PatientChartSet";
 import PatientPageSelectBox from "@components/PatientPageSelectBox";
 import PatientTable from "@components/PatientTable";
+import { fetchChartStats } from "@modules/chartStats";
 import { fetchFilterList } from "@modules/filterList";
 import { fetchPatients } from "@modules/patient";
 import { useDispatch } from "react-redux";
@@ -11,6 +14,8 @@ function App() {
   const { dataList, page, length, totalLength } = useSelector(
     (state) => state.patientReducer
   );
+  const { filterList } = useSelector((state) => state.filterListReducer);
+  const { stats } = useSelector((state) => state.chartStatsReducer);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -22,17 +27,23 @@ function App() {
     );
 
     dispatch(fetchFilterList());
+    dispatch(fetchChartStats());
   }, [dispatch]);
 
   return (
     <div className="App">
-      <PatientPageSelectBox page={page} />
-      <PatientTable
-        data={dataList}
-        page={page}
-        length={length}
-        pageCnt={Math.ceil(totalLength / length)}
-      />
+      <Container className="d-flex">
+        <PatientChartSet filterList={filterList} stats={stats} />
+      </Container>
+      <Container>
+        <PatientPageSelectBox page={page} />
+        <PatientTable
+          data={dataList}
+          page={page}
+          length={length}
+          pageCnt={Math.ceil(totalLength / length)}
+        />
+      </Container>
     </div>
   );
 }
