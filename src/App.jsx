393fcdcf305aa10e992bea1936d228415eb5f506
이태희 <leetaehee0205@gmail.com>
2021-09-24 12:36:23 +0900

    add : connect patient category, data and DataTable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import DataTable from "./components/DataTable";
+import { fetchPatients } from "./modules/patient";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 function App() {
-  return <div className="App"></div>;
+  const { loading, dataList, page, error } = useSelector(
+    (state) => state.patientReducer
+  );
+  const dispatch = useDispatch();
+
+  const patientsCategory = {
+    personID: "환자 ID",
+    gender: "성별",
+    birthDatetime: "생년월일",
+    age: "나이",
+    race: "인종",
+    ethnicity: "민족",
+    isDeath: "사망 여부",
+  };
+
+  useEffect(() => {
+    dispatch(
+      fetchPatients({
+        page: 1,
+        length: 20,
+      })
+    );
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <DataTable
+        categories={patientsCategory}
+        data={dataList}
+        keyID="personID"
+      />
+    </div>
+  );
 }
 
 export default App;
