9cc7d26f422ba06b4941d2d43bc0d5951ca2b2d6
이태희 <leetaehee0205@gmail.com>
2021-09-24 20:58:45 +0900

    create : filter list state

---
+++ src/modules/filterList.js
@@ -0,0 +1,90 @@
+import { getEthnicityList, getGenderList, getRaceList } from "@apis/filterList";
+
+const LOADING = "filterList/FILTER_LIST_LOADING";
+const FILTER_LIST_FETCH = "filterList/FILTER_LIST_FETCH";
+const FILTER_CHECK = "filterList/FILTER_CHECK";
+const ERROR = "filterList/FILTER_LIST_ERROR";
+
+// 필터 리스트 관리
+export const fetchFilterList = () => async (dispatch) => {
+  try {
+    dispatch({ type: LOADING });
+
+    const gender = await getGenderList();
+    const race = await getRaceList();
+    const ethnicity = await getEthnicityList();
+    const isDeathList = ["T", "F"];
+
+    dispatch({
+      type: FILTER_LIST_FETCH,
+      filterList: { ...gender, ...race, ...ethnicity, isDeathList },
+    });
+  } catch (e) {
+    dispatch({ type: ERROR, error: e });
+  }
+};
+
+/**
+ * 필터 리스트 선택 값 관리
+ *
+ * @param {String} category 필터 카테고리
+ * @param {String} value 선택한 필터 값
+ */
+export const checkFilter = (category, value) => {
+  let obj = {};
+  obj[category] = value;
+
+  return { type: FILTER_CHECK, check: obj };
+};
+
+const initialState = {
+  loading: false,
+  filterList: {
+    genderList: [],
+    raceList: [],
+    ethnicityList: [],
+    isDeathList: [],
+  },
+  checkVal: {
+    gender: "",
+    minAge: 0,
+    maxAge: 200,
+    race: "",
+    ethnicity: "",
+    isDeath: "",
+  },
+  error: false,
+};
+
+export default function filterListReducer(state = initialState, action) {
+  switch (action.type) {
+    case LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FILTER_LIST_FETCH:
+      return {
+        ...state,
+        loading: false,
+        filterList: action.filterList,
+        error: false,
+      };
+    case FILTER_CHECK:
+      return {
+        ...state,
+        checkVal: {
+          ...state.checkVal,
+          ...action.check,
+        },
+      };
+    case ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.error.message,
+      };
+    default:
+      return state;
+  }
+}
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import filterListReducer from "@modules/filterList";
 import patientReducer from "@modules/patient";
 
 const rootReducers = combineReducers({
   patientReducer,
+  filterListReducer,
 });
 
 export default rootReducers;
